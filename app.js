42a54e025efd4c9970fd8e05a9eeea507c2d64c3
Firecreper2 <64336537+Firecreper2@users.noreply.github.com>
2021-02-28 20:19:52 -0500

    --- Auto-Git Commit ---

---
+++ app.js
@@ -0,0 +1,17 @@
+//const PORT = 5500
+const web = './static/web.html'
+const express = require("express");
+const fs = require('fs')
+const site = '/static/index.html'
+
+const app = express();
+app.use(express.static('static'))
+
+app.get('/', function(req, res)  {
+  res.sendFile(__dirname + site);
+});
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Serving ${web} on port ${process.env.PORT} at localhost`);
+});
